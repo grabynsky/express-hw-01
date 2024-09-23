42e193121186958cf6336e484dd7a8826ce6257d
Andrew Grabynsky <hrabynsky@gmail.com>
2024-09-23 19:10:57 +0300

    task

---
+++ fs.service.js
@@ -0,0 +1,17 @@
+const path = require("node:path");
+const fs = require("node:fs/promises");
+
+module.exports = {
+    read: async () => {
+        const pathToFile = path.join(__dirname, 'db.json');
+        const data = await fs.readFile(pathToFile, "utf-8");
+
+        return data ? JSON.parse(data) : [];
+    },
+
+    write: async (users) => {
+        const pathToFile = path.join(__dirname, 'db.json');
+
+        await fs.writeFile(pathToFile, JSON.stringify(users))
+    }
+}
